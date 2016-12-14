6adac5b7d37ca1f455e71a91415512429f5d348a
Andrew Campebll <acampbell@saucelabs.com>
2016-12-14 14:25:07 -0500

    Update test_saucelabs_web.py
    minor code cleanup and added a check for 'manual session button' in the dashboard test

--- tests/test_saucelabs_web.py
+++ tests/test_saucelabs_web.py
@@ -11,14 +11,12 @@ class SaucelabsWebTests(BaseTest):
     #Verify that google.com can be reached
     def test_googleHomePage(self):
         self.driver.get('https://google.com/')
-        title = "Google"
-        self.assertEqual(title, self.driver.title, "google is not locding correclty")
+        self.assertEqual("Google", self.driver.title, "google is not locding correclty")
 
     #Verify that SauceLabs.com can be reached
     def test_sauceLabsHomePage(self):
         self.driver.get('https://saucelabs.com/')
-        title = "Cross Browser Testing, Selenium Testing, and Mobile Testing | Sauce Labs"
-        self.assertEqual(title, self.driver.title, "sauce labs is not loading correctly")
+        self.assertEqual("Cross Browser Testing, Selenium Testing, and Mobile Testing | Sauce Labs", self.driver.title, "sauce labs is not loading correctly")
 
     #Verify that a user can login to the service and reach it's dashboard
     def test_sauceLabsDashboard(self):
@@ -27,9 +25,10 @@ class SaucelabsWebTests(BaseTest):
         self.driver.find_element_by_name('username').send_keys('testybesty10')
         self.driver.find_element_by_name('password').send_keys('testybesty10')
         self.driver.find_element_by_id('submit').click()
+        #Verify 'new manual session' button is present, this ensures the page has completed loading before verifying the title
+        self.driver.find_element_by_name('icn-tabbar-manual')
         #verify the title of the dashboard is present and correct
-        title = "Sauce Labs | Dashboard"
-        self.assertEqual(title, self.driver.title, "Dashboard is not loading correctly")
+        self.assertEqual("Sauce Labs | Dashboard", self.driver.title, "Dashboard is not loading correctly")
 
 if __name__ == '__main__':
     unittest.main()
